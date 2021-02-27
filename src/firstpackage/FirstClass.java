69fe8fb498a6a96a3dc85cc647572e8d229abad9
sharopovbehzod <sharopovbehzod@gmail.com>
2021-02-27 08:36:01 -0500

    first commit from local master

---
+++ src/firstpackage/FirstClass.java
@@ -0,0 +1,15 @@
+package firstpackage;
+
+public class FirstClass {
+	
+	public static void main(String[] args) {
+		System.out.println("First comment from local master");
+		
+		
+		
+	}
+	
+	
+	
+
+}
