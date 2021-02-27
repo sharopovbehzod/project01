fd720ca47b793642b13f100b154a198544809d4a
sharopovbehzod <sharopovbehzod@gmail.com>
2021-02-27 09:10:43 -0500

    second commit from local master

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -4,7 +4,7 @@ public class FirstClass {
 	
 	public static void main(String[] args) {
 		System.out.println("First comment from local master");
-		
+		System.out.println("Second line of the code i am typing now");
 		
 		
 	}
